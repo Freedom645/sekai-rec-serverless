0a8d62d2e34398af84d6a9328863fa4421f656a3
Freedom645 <strikefreedom.kou@gmail.com>
2023-10-21 19:58:04 +0900

    lint修正、dotenv導入

--- serverless.ts
+++ serverless.ts
@@ -1,14 +1,16 @@
 import type { AWS } from '@serverless/typescript';
 
-import hello from '@functions/hello';
+import musicDataUpdater from '@functions/music-data-updater';
 
 const serverlessConfiguration: AWS = {
   service: 'sekai-rec-serverless',
   frameworkVersion: '3',
-  plugins: ['serverless-esbuild'],
+  plugins: ['serverless-esbuild', 'serverless-dotenv-plugin'],
+  useDotenv: true,
   provider: {
     name: 'aws',
     runtime: 'nodejs14.x',
+    stage: '${opt:stage, self:custom.defaultStage}',
     apiGateway: {
       minimumCompressionSize: 1024,
       shouldStartNameWithService: true,
@@ -16,13 +18,53 @@ const serverlessConfiguration: AWS = {
     environment: {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
       NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
+      S3_BUCKET: '${env:S3_BUCKET}',
+      SEKAI_MASTER_URL: '${env:SEKAI_MASTER_URL}',
     },
     region: 'ap-northeast-1',
   },
-  // import the function via paths
-  functions: { hello },
+  functions: {
+    musicDataUpdater,
+  },
   package: { individually: true },
+  resources: {
+    Resources: {
+      Bucket: {
+        Type: 'AWS::S3::Bucket',
+        Properties: {
+          BucketName: '${self:provider.environment.S3_BUCKET}',
+        },
+      },
+      S3IamPolicy: {
+        Type: 'AWS::IAM::Policy',
+        DependsOn: ['Bucket'],
+        Properties: {
+          PolicyName: 'lambda-s3',
+          PolicyDocument: {
+            Version: '2012-10-17',
+            Statement: [
+              {
+                Effect: 'Allow',
+                Action: ['s3:GetObject', 's3:PutObject', 's3:DeleteObject'],
+                Resource: 'arn:aws:s3:::${self:provider.environment.S3_BUCKET}/*',
+              },
+              {
+                Effect: 'Allow',
+                Action: ['s3:ListBucket'],
+                Resource: 'arn:aws:s3:::${self:provider.environment.S3_BUCKET}',
+              },
+            ],
+          },
+          Roles: [{ Ref: 'IamRoleLambdaExecution' }],
+        },
+      },
+    },
+  },
   custom: {
+    defaultStage: 'dev',
+    dotenv: {
+      path: 'env',
+    },
     esbuild: {
       bundle: true,
       minify: false,
